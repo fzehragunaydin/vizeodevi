9fd23d0ea9cd3ffc5153c0662bdfdbe91b9d1fca
zehra <toprakmax@hotmail.com>
2021-04-23 23:27:52 +0300

    veriyi çektim, butonun içine yazdırdım

--- vizeodevi/Form1.Designer.cs
+++ vizeodevi/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace vizeodevi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnxmlyazdır = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnxmlyazdır
+            // 
+            this.btnxmlyazdır.Location = new System.Drawing.Point(266, 35);
+            this.btnxmlyazdır.Name = "btnxmlyazdır";
+            this.btnxmlyazdır.Size = new System.Drawing.Size(405, 33);
+            this.btnxmlyazdır.TabIndex = 0;
+            this.btnxmlyazdır.Text = "YOL DURUMU";
+            this.btnxmlyazdır.UseVisualStyleBackColor = true;
+            this.btnxmlyazdır.Click += new System.EventHandler(this.btnxmlyazdır_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1047, 651);
+            this.Controls.Add(this.btnxmlyazdır);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnxmlyazdır;
     }
 }
 
--- vizeodevi/Form1.cs
+++ vizeodevi/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace vizeodevi
 {
     public partial class Form1 : Form
     {
+        string yoldurumubultenLink = "http://95.0.225.161/yolDurumWS/yolDurumuRss.aspx"; 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnxmlyazdır_Click(object sender, EventArgs e)
+        {
+            XmlDocument veri = new XmlDocument();
+           veri.Load(yoldurumubultenLink);
+            XmlElement root = veri.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("items");
+
+            foreach (XmlNode node in nodes)
+            {
+                string konum = node["tittle"].InnerText;
+                string aciklama = node["description"].InnerText;
+                string site = node["link"].InnerText;
+
+            }
+
+
         }
     }
 }
