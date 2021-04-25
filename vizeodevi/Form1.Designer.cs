21148301e600b293aaa8da70b738256cd47773bb
zehra <toprakmax@hotmail.com>
2021-04-25 07:19:54 +0300

    düzenleme:' yazdirma' adında public adında oluşturdum ardından butona ekledim,timer ekledim(1 saatlik )

--- vizeodevi/Form1.Designer.cs
+++ vizeodevi/Form1.Designer.cs
@@ -29,10 +29,14 @@ namespace vizeodevi
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnxmlyazdır = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnxmlyazdır
@@ -45,55 +49,68 @@ namespace vizeodevi
             this.btnxmlyazdır.UseVisualStyleBackColor = true;
             this.btnxmlyazdır.Click += new System.EventHandler(this.btnxmlyazdır_Click);
             // 
-            // label1
+            // dataGridView1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(266, 135);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(127, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(704, 390);
+            this.dataGridView1.TabIndex = 4;
             // 
-            // label2
+            // Column1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(266, 193);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.Column1.HeaderText = "bolge";
+            this.Column1.MinimumWidth = 50;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
             // 
-            // label3
+            // Column2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(266, 253);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.Column2.HeaderText = "durum";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "link";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3600;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1047, 651);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnxmlyazdır);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnxmlyazdır;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- vizeodevi/Form1.cs
+++ vizeodevi/Form1.cs
@@ -18,31 +18,38 @@ namespace vizeodevi
             InitializeComponent();
         }
 
-        public void btnxmlyazdır_Click(object sender, EventArgs e)
+        public void yazdirma()
         {
-
-            {
-
                 XmlDocument doc1 = new XmlDocument();
-                doc1.Load("http://95.0.225.161/yolDurumWS/yolDurumuRss.aspx");
+        doc1.Load("http://95.0.225.161/yolDurumWS/yolDurumuRss.aspx");
                 XmlElement root = doc1.DocumentElement;
-                XmlNodeList nodes = root.SelectNodes("channel/item");
+        XmlNodeList nodes = root.SelectNodes("channel/item");
 
                 foreach (XmlNode node in nodes)
                 {
                     string bolge = node["title"].InnerText;
-                    string durum = node["description"].InnerText;
-                    string link = node["link"].InnerText;
+        string durum = node["description"].InnerText;
+        string link = node["link"].InnerText;
 
-                    label1.Text = bolge;
-                    label2.Text = durum;
-                    label3.Text = link;
+        DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+        row.Cells[0].Value = bolge;
+                    row.Cells[1].Value = durum;
+                    row.Cells[2].Value = link;
+                    dataGridView1.Rows.Add(row);
                 }
+}
 
+    public void btnxmlyazdır_Click(object sender, EventArgs e)
+        {
+            yazdirma();
+        }
 
-        }   }
-
-
-}   }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 3600;
+            yazdirma();
+        }
+    }   
+}
             
 
